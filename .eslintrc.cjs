a7012d8cbf222905342f83630bb645ce32cd750b
Preston DMV <preston.davies@dmv.ca.gov>
2023-06-22 15:10:07 -0400

    Basic Setup with ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,27 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'plugin:react-hooks/recommended',
+		'@10up/eslint-config/react',
+	],
+	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+	settings: {
+		react: { version: '18.2' },
+	},
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': 'warn',
+		'react/prop-types': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+			},
+		],
+	},
+};
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <div>Test Data</div>
-      </div>
-    </>
-  );
-}
+const App = () => {
+	return (
+		<div className="background">
+			<h1>Vite + React</h1>
+			<div className="card">
+				<div className="headline">Test Data</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
